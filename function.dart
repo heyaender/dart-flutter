afb2a039d29aa9e5e413cc15da5731670d6fdcd6
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-06-07 15:22:25 +0700

    function

---
+++ function.dart
@@ -0,0 +1,13 @@
+/// PrintInteger() takes an integer as an argument and prints it to the console.
+/// 
+/// Args:
+///   aNumber (int): This is the name of the parameter.
+void printInteger(int aNumber) {
+  print('The number is $aNumber');
+}
+
+/// The printInteger() function takes an integer as an argument and prints it to the console.
+void main() {
+  var number = 77;
+  printInteger(number);
+}
--- variable.dart
+++ variable.dart
@@ -1,6 +1,6 @@
 void main() {
   var name = 'Voyager I'; //string
-  var year = 1977; //interger
+  var year = 1996; //interger
   var antennaDiameter = 3.7; //double
   var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune']; //list
   var image = {
@@ -8,9 +8,11 @@ void main() {
     'url': '//path/to/saturn.jpg'
   }; //map
   var isLoggedIn = true; //bool
+  var age = 2022 - year;
 
   print('The name is $name');
-  print('The year is $year');
+  print('The year of my birth $year');
+  print('My Age is $age');
   print('The diameter is ${antennaDiameter * 100} cm');
   print('The flyby objects are: ${flybyObjects[2]}');
   print('The image is ${image['url']}');
