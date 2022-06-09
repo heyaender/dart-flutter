7e362a4bc973e2d35ee8e61bc6de29a2f3992f79
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-06-09 15:01:02 +0700

    Materi Dart - Kondisi

---
+++ if-else.dart
@@ -0,0 +1,12 @@
+void main() {
+  int angka = 2;
+  if(angka == 0) {
+    print('Nol');
+  } else {
+    if(angka % 2 == 0) {
+      print("Angka $angka adalah bilangan genap");
+    } else {
+      print("Angka $angka adalah bilangan ganjil");
+    }
+  }
+}
