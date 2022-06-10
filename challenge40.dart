cd6505fbd716308f355b80a10442aaee8bbbf8dc
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-06-10 11:38:41 +0700

    Update challenge40.dart

--- challenge40.dart
+++ challenge40.dart
@@ -21,6 +21,7 @@ void main(List<String> args) {
   var n;
   stdout.write('Masukkan N = ');
   n = int.parse(stdin.readLineSync()!);
+  /// Printing a triangle of stars.
   for (int i = 0; i < n; i++) {
     for (int j = 0; j <= i; j++) {
       stdout.write('*');
